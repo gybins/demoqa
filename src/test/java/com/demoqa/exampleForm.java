acd6be0d7c7633a290f817485fdb48dca681f4df
Sergei <gybins@yandex.ru>
2024-07-29 15:08:19 +0500

    add checks

--- src/test/java/com/demoqa/exampleForm.java
+++ src/test/java/com/demoqa/exampleForm.java
@@ -23,7 +23,7 @@ public class exampleForm {
         String firstName = "Sergei";
         String lastName = "Gubin";
         String email = "gybins@yandex.ru";
-        String mobileNumber = "77074331232";
+        String mobileNumber = "7707433123";
         String gender = "Male";
         String month = "November";
         String year = "1996";
@@ -31,8 +31,8 @@ public class exampleForm {
         String subject2 = "Sports";
         String currentAddress = "Almaty";
         String nameFile = "image.png";
-        String state = "null";
-        String city = "Almaty";
+        String state = "NCR";
+        String city = "Noida";
 
         open("/automation-practice-form");
         $(".text-center").shouldHave(text("Practice Form"));
@@ -58,6 +58,28 @@ public class exampleForm {
         File file = new File("src/resources/" +nameFile);
         $("#uploadPicture").uploadFile(file);
         $("#uploadPicture").shouldHave(value(nameFile));
+
+        $("#currentAddress").sendKeys(currentAddress);
+        $(byText("Select State")).click();
+        $(byText(state)).click();
+        $(byText("Select City")).click();
+        $(byText(city)).click();
+
+        $("#submit").click();
+
+        //CHECK
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $(".table").$(byText("Student Name")).sibling(0).shouldHave(text(firstName + " " + lastName));
+        $(".table").$(byText("Student Email")).sibling(0).shouldHave(text(email));
+        $(".table").$(byText("Gender")).sibling(0).shouldHave(text(gender));
+        $(".table").$(byText("Mobile")).sibling(0).shouldHave(text(mobileNumber));
+        $(".table").$(byText("Date of Birth")).sibling(0).shouldHave(text("25 "+ month + "," + year));
+        $(".table").$(byText("Subjects")).sibling(0).shouldHave(text(subject1));
+        $(".table").$(byText("Hobbies")).sibling(0).shouldHave(text(subject2));
+        $(".table").$(byText("Picture")).sibling(0).shouldHave(text(nameFile));
+        $(".table").$(byText("Address")).sibling(0).shouldHave(text(currentAddress));
+        $(".table").$(byText("State and City")).sibling(0).shouldHave(text(state + " " + city));
+
 
 
 
